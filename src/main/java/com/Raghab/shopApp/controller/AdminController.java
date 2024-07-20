e85c589854adbbae94a5e3c8e63f0a702f6aa797
raghab007 <pokhrelraghab3@gmail.com>
2024-07-20 15:01:00 +0545

    AdminController modified

--- src/main/java/com/Raghab/shopApp/controller/AdminController.java
+++ src/main/java/com/Raghab/shopApp/controller/AdminController.java
@@ -1,44 +1,86 @@
 package com.Raghab.shopApp.controller;
+import com.Raghab.shopApp.entity.Orders;
 import com.Raghab.shopApp.entity.Product;
+import com.Raghab.shopApp.repository.OrderRepository;
 import com.Raghab.shopApp.service.AdminService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.Raghab.shopApp.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.HashMap;
+import java.util.List;
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/admin")
 public class AdminController {
 
-    @Autowired
+
     AdminService productService;
 
-    @GetMapping
-    public ResponseEntity<?>  getAllProduct(){
-        return new ResponseEntity<>(productService.getProducts(),HttpStatus.FOUND);
+    UserService userService;
+
+    OrderRepository orderRepository;
+    public AdminController(AdminService adminService, UserService userService, OrderRepository orderRepository){
+        this.productService = adminService;
+        this.userService = userService;
+        this.orderRepository = orderRepository;
     }
-    @PostMapping
-    public ResponseEntity<?> addProduct(@RequestBody Product product){
+
+    @GetMapping("product")
+    public ResponseEntity<?> getAllProduct() {
+        return new ResponseEntity<>(productService.getProducts(), HttpStatus.FOUND);
+    }
+    @PostMapping("product")
+    public ResponseEntity<?> addProduct(@Valid  @RequestBody Product product) {
         productService.addProduct(product);
-        return  new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
-    @DeleteMapping("/{productId}")
-    public ResponseEntity<?> deleteById(@PathVariable() Long productId){
-      if(productService.removeById(productId)){
-          return new ResponseEntity<>(HttpStatus.OK);
-      }
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    @DeleteMapping("/delete-product/{productId}")
+    public ResponseEntity<?> deleteById(@PathVariable Long productId) {
+        if (productService.removeById(productId)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-    @PutMapping("/{productId}")
-    public ResponseEntity<?> updateById(@PathVariable Long productID, @RequestBody Product product){
+
+
+    @PutMapping("/update-product/{productId}")
+    public ResponseEntity<?> updateById(@PathVariable Long productID, @RequestBody Product product) {
 
         boolean saved = productService.updateById(productID, product);
-        if (saved){
+        if (saved) {
             return new ResponseEntity<>(HttpStatus.OK);
-
         }
-
-        return  new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+        return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
+    @GetMapping("/registered-users")
+        public ResponseEntity<?> getAllRegisteredUsers(){
+            return new ResponseEntity<>(userService.getAll(),HttpStatus.OK);
+        }
+    @DeleteMapping("/delete-user/{userName}")
+    public boolean removeUser(@PathVariable String userName){
+        return userService.deleteByUserName(userName);
+    }
+
+    @GetMapping
+    public List<Orders> getAllOrders(){
+        return  orderRepository.findAll();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> methodArgumentNotValidException(MethodArgumentNotValidException e){
+        var errors = new HashMap<String,String>();
+        e.getBindingResult().getAllErrors().forEach(error->{
+            var fieldName =( (FieldError)error).getField();
+            var errorMessage = error.getDefaultMessage();
+            errors.put(fieldName,errorMessage);
+        });
+
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+    }
+
 
 }
